0ff31c6837fcf97adcf6403ce641db9df9bea6b5
nicko-535200065 <nicko.535200065@stu.untar.ac.id>
2024-11-05 13:25:07 +0000

    1

--- pages/5_Date_Clustering.py
+++ pages/5_Date_Clustering.py
@@ -52,7 +52,7 @@ def run_kmeans(df, n_clusters=3):
 df = load_data()
 
 # Menjalankan K-Means Clustering
-st.header("K-Means Clustering dengan Silhouette Score")
+st.header("K-Means Clustering untuk Tanggal")
 n_clusters = st.slider("Pilih jumlah cluster:", 2, 10, 3)
 clustered_df, silhouette_avg = run_kmeans(df, n_clusters)
 
