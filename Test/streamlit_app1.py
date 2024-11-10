ee179e0841194131d13883dc9dd17a46adc25d2d
nicko-535200065 <nicko.535200065@stu.untar.ac.id>
2024-11-10 12:39:21 +0000

    1

--- Test/streamlit_app1.py
+++ Test/streamlit_app1.py
@@ -1,9 +1,9 @@
-import streamlit as st
+#import streamlit as st
 
-st.set_page_config(
-    page_title="Streamlit",
-    page_icon="",
-)
+#st.set_page_config(
+#    page_title="Streamlit",
+#   page_icon="",
+#)
 
 # Redirect langsung ke halaman Home
-st.switch_page("pages/1_Home.py")
+#st.switch_page("pages/1_Home.py")
